d7dcb9e9cf9bb4f9a9842f80b7e8af4db10e39f4
josiahaltschuler <josiah.altschuler@gmail.com>
2019-06-13 09:47:51 -0500

    changed mlucas to altschuler

--- checkin_novogene_gbs_data.sh
+++ checkin_novogene_gbs_data.sh
@@ -4,8 +4,8 @@
 # $1 is the GBS folder in /bulk/altschuler/incoming to process. For example, C202SC19050314
 #
 date
-cd /bulk/mlucas/incoming/$1/
-gbsFolders=$(find /bulk/mlucas/incoming/$1/raw_data -maxdepth 1 -name "GBS*" )
+cd /bulk/altschuler/incoming/$1/
+gbsFolders=$(find /bulk/altschuler/incoming/$1/raw_data -maxdepth 1 -name "GBS*" )
 echo $gbsFolders
 #
 # Process each GBS folder
@@ -23,26 +23,26 @@ for file in $gbsFolders; do
    echo "Renaming GBS file and updating flowcell and lane in gbs table"
    echo "Processing GBS folder: "$file
    date
-   /homes/mlucas/python3_programs/GBS/rename_gbs_file -p $file -s novogene
+   /homes/altschuler/python3_programs/GBS/rename_gbs_file -p $file -s novogene
    date
    echo "Generating QC reports for  GBS File"
-   find . -name "*R1*fastq.txt.gz"| xargs -I {} /homes/mlucas/python3_programs/GBS/generate_barcode_distribution-V04 -i {}
+   find . -name "*R1*fastq.txt.gz"| xargs -I {} /homes/altschuler/python3_programs/GBS/generate_barcode_distribution-V04 -i {}
    echo "Generating DNA Quantification report"
    gbsFile="$(basename "$file")"
    gbsNumber=$(echo "${gbsFile}" | cut -c1-7)
    echo $gbsNumber
    date
-   /homes/mlucas/python3_programs/GBS/dna_quantification_report -g $gbsNumber
+   /homes/altschuler/python3_programs/GBS/dna_quantification_report -g $gbsNumber
    echo "QC Completed"
    echo "Calculating MD5 and line counts for  GBS file"
    date
-   find . -name "*fastq.txt.gz"| xargs -I {} /homes/mlucas/python3_programs/GBS/compute_gbs_file_table_metadata -p {}
+   find . -name "*fastq.txt.gz"| xargs -I {} /homes/altschuler/python3_programs/GBS/compute_gbs_file_table_metadata -p {}
    date
    echo "Moving  GBS files to sequence archive"
    find . -name "*fastq.txt.gz"| xargs -I {} chgrp ksu-plantpath-jpoland {}
    find . -name "*fastq.txt.gz"| xargs -I {} chmod a-w {}
    find . -name "*R1*fastq.txt.gz"| xargs -I {} mv {} /bulk/jpoland/sequence/.
-   find . -name "*R2*fastq.txt.gz"| xargs -I {} mv {} /bulk/mlucas/PE_sequence/R2_files/.
+   find . -name "*R2*fastq.txt.gz"| xargs -I {} mv {} /bulk/altschuler/PE_sequence/R2_files/.
 date
 done
 echo "QC Completed"
