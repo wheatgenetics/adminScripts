5d06bf70bcfe6aa995a7c46e15ceb1c6669f1c53
josiahaltschuler <josiah.altschuler@gmail.com>
2020-03-20 16:16:35 -0500

    goes through each subfolder and runs md5 checks. outputs check to subfolder

--- checkin_psomagen_gbs_data.sh
+++ checkin_psomagen_gbs_data.sh
@@ -13,3 +13,31 @@ folders=$(find /bulk/jpoland/raw_data/psomagen/$1 -maxdepth 1 -name "GBS*")
 echo
 echo 'Found the following folders to process:'
 printf '%s\n' "${folders[@]}"
+
+#
+# Process each GBS folder
+#
+for folder in $folders; do
+  cd $folder
+  echo
+  echo '********************'
+  date
+  echo
+  echo "Processing GBS folder:"
+  echo $folder
+  echo
+  md5name=$(basename $folder).md5
+  echo "Verifying checksums in ${md5name}:"
+  $(md5sum -c ${md5name} > ${md5name}.checked)
+  # md5CheckResult=$(md5sum -c ${md5name})
+  # printf '%s\n' "${md5CheckResult[@]}"
+
+  if [[ $md5CheckResult == *"FAIL"* ]]; then
+    echo "MD5 Check Failed"
+  fi
+done
+
+echo
+echo "Check-in completed"
+
+exit
