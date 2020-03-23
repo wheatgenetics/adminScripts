906ca9ee81d3f37c254b49ca50443eef5eefd8f8
josiahaltschuler <josiah.altschuler@gmail.com>
2020-03-23 11:33:35 -0500

    changes all files to read only and group ownership to ksu-plantpath-jpoland recursively at the beginning of script

--- checkin_psomagen_gbs_data.sh
+++ checkin_psomagen_gbs_data.sh
@@ -13,10 +13,17 @@ folders=$(find /bulk/jpoland/raw_data/psomagen/$1 -maxdepth 1 -name "GBS*")
 echo
 echo 'Found the following folders to process:'
 printf '%s\n' "${folders[@]}"
+echo
 
-#
+# Make all files read only
+echo 'Changing all file permissions to read only (444)'
+$(chmod -R 0444 .)
+
+# Change group ownership to ksu-plantpath-jpoland for all files and folders
+echo 'Changing group ownership to ksu-plantpath-jpoland for all files and folders'
+$(chgrp -R ksu-plantpath-jpoland .)
+
 # Process each GBS folder
-#
 for folder in $folders; do
   cd $folder
   echo
@@ -33,9 +40,6 @@ for folder in $folders; do
   if [[ $md5CheckResult == *"FAIL"* ]]; then
     echo "MD5 Check Failed"
   fi
-
-  # Make all files read only
-  $(chmod 0444 *)
 done
 
 echo
