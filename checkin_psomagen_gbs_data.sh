958456292cd651f8c687e148097b2e121a55e409
josiahaltschuler <josiah.altschuler@gmail.com>
2020-04-06 13:44:20 -0500

    added the python renamingscript to the psomagen checkin bash script

--- checkin_psomagen_gbs_data.sh
+++ checkin_psomagen_gbs_data.sh
@@ -37,6 +37,12 @@ for folder in $folders; do
     echo "MD5 Check Failed"
   fi
 
+  echo
+  echo "Renaming GBS files and updating flowcell and lane in gbs table:"
+  python /homes/altschuler/GBS/rename_gbs_file_and_update_flowcell_and_name_in_database.py -p $folder -s psomagen
+  echo
+
+
   # Make all files read only
   echo 'Changing all file permissions to read only (444)'
   $(chmod 0444 *)
